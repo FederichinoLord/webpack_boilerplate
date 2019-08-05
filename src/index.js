5f04e974cb85a6bf27ba341b5ce70a7f4ca08a3a
Federico Piccinini <federicopiccinini@Federicos-MacBook-Pro.local>
2019-08-05 20:56:36 +0100

    sortable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log("hello from webpack")
+
+import Sortable from 'sortablejs';
+console.log(Sortable);
+var el = document.getElementById('items');
+var sortable = Sortable.create(el);
+console.log(el);
